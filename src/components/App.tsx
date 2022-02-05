e2f86130039f50058b877d5bc1df0f5d6e5ed02a
Maxi <maximilianoarrettino@gmail.com>
2022-02-05 19:20:00 -0300

    Finish initial architecture of the app

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../scss/app.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Edit src/App.tsx and save to reload.</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
