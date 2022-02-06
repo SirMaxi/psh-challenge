cf0bb37a713201f3b1cc345d11c0fe7f1aabf6f4
Maxi <maximilianoarrettino@gmail.com>
2022-02-06 13:20:45 -0300

    Add redux architecture

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import '../scss/app.scss'
+import { Provider } from 'react-redux'
+import store from 'redux/store'
 import ChatScreen from './ChatScreen'
 
 const App: React.FC = () => {
-  return <ChatScreen />
+  return (
+    <Provider store={store}>
+      <ChatScreen />
+    </Provider>
+  )
 }
 
 export default App
